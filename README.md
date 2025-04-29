cc7a6f4604bbd342526d21a7c8245b5e851e54c0
Filip Grela <fgrela09@gmail.com>
2025-04-29 11:48:28 +0200

    Add menu

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Systemy-Operacyjne-du-e-skrypty
+# Systemy-Operacyjne-duze-skrypty
+
+# Skrypt 1
+## System Information Script
+
+### Opis projektu
+Skrypt pozwala na wyświetlanie szczegółowych informacji o systemie, takich jak:
+- Procesor, pamięć, przestrzeń dyskowa i inne dane systemowe.
+- Stan systemu: zużycie procesora, pamięci, dysku itp.
+- Dynamiczna prezentacja danych z możliwością okresowego odświeżania.
+- Możliwość zapisania informacji do raportu w pliku, na podstawie danych z wybranego okresu czasu.
--- skrypt.sh
+++ skrypt.sh
@@ -9,3 +9,37 @@
 #
 # Licensed under GPL (see /usr/share/common-licenses/GPL for more details
 # or contact # the Free Software Foundation for a copy)
+
+#!/bin/bash
+
+# === KONFIGURACJA ===
+REPORT_FILE="raport_systemowy.txt"
+REFRESH_INTERVAL=0.2   # sekundy między odświeżeniami (dla trybu dynamicznego)
+
+# === FUNKCJE ===
+
+function show_menu() {
+    echo "=== MENU ==="
+    echo "1) Wyświetl informacje systemowe"
+    echo "2) Zapisz raport do pliku"
+    echo "3) Tryb dynamiczny (monitor na żywo)"
+    echo "0) Wyjście"
+    
+    read -p "Wybierz opcję: " choice
+    
+
+    # Call the function for the selected option
+    case $choice in
+        1) basic_info ;;
+        2) save_report ;;
+        3) dynamic_mode ;;
+        0) exit 0 ;;
+        *) clear; echo "Niepoprawny wybór!"; sleep 1;;
+    esac
+}
+
+while true; do
+    clear
+    show_menu
+    
+done
