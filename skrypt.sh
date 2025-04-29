05688b854f942bf2ecfa77671fe745dcf3310cc0
Filip Grela <fgrela09@gmail.com>
2025-04-29 11:33:46 +0200

    Init

---
+++ skrypt.sh
@@ -0,0 +1,11 @@
+# Author           : Imie Nazwisko ( Filip Grela )
+# Created On       : 29.04.2025
+# Last Modified By : Imie Nazwisko ( fgrela09@gmail.com )
+# Last Modified On : 29.04.2025 
+# Version          : 0.1
+#
+# Description      :
+# Opis
+#
+# Licensed under GPL (see /usr/share/common-licenses/GPL for more details
+# or contact # the Free Software Foundation for a copy)
